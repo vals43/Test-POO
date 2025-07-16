e50c374dc4f9de1fccb14f75aca6d8aa26f51508
sambatra <hei.sambatra@gmail.com>
2025-07-16 11:30:39 +0300

    the moves in the game

--- src/main/java/com/monorganisation/monprojet/Game.java
+++ src/main/java/com/monorganisation/monprojet/Game.java
@@ -1,5 +1,42 @@
 package com.monorganisation.monprojet;
 
+import java.util.Scanner;
+
 public class Game {
+    private static SpaceShip spaceShip;
+
+    public void move() {
+        int i = 0;
+        while (i < 20) {
+            Scanner move = new Scanner(System.in);
+            System.out.println("Enter move (U/D/L/R): ");
+            String yourMove = move.nextLine();
+
+            switch (yourMove) {
+                case "U":
+                    spaceShip.setY(1);
+                    i++;
+                    break;
+                case "D":
+                    spaceShip.setY(-1);
+                    i++;
+                    break;
+                case "L":
+                    spaceShip.setX(-1);
+                    i++;
+                    break;
+                case "R":
+                    spaceShip.setX(1);
+                    i++;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid move, please try again");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
