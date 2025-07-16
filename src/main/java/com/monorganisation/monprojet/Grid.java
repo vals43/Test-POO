0c59915c221979382a43f7a8053e77348c2844fc
Dr Herilala <hei.teddy.3@gmail.com>
2025-07-16 11:15:11 +0300

    add : methode in Grid

--- src/main/java/com/monorganisation/monprojet/Grid.java
+++ src/main/java/com/monorganisation/monprojet/Grid.java
@@ -1,5 +1,27 @@
 package com.monorganisation.monprojet;
 
+import java.util.Arrays;
+
 public class Grid {
+
+     static char[][] initGrid(int size, int Junk){
+        char[][] matrice = new char[size][size];
+         for (int i = 0; i < size; i++) {
+             Arrays.fill(matrice[i], '.');
+         }
+         for (int i = 0; i < Junk; i++) {
+             int X = (int)(Math.random() * size);
+             int Y = (int)(Math.random() * size);
+             matrice[X][Y] = 'J';
+         }
+        return matrice;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(initGrid(5, 5)));
+    }
+
+
+
 
 }
--- src/main/java/com/monorganisation/monprojet/SpaceShip.java
+++ src/main/java/com/monorganisation/monprojet/SpaceShip.java
@@ -1,5 +1,28 @@
 package com.monorganisation.monprojet;
 
 public class SpaceShip {
+    private int x;
+    private int y;
+
+    public SpaceShip() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x += x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y += y;
+    }
 
 }
