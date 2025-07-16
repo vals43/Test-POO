29353d8e667a33053d03be494c74d7eaf900b103
Dr Herilala <hei.teddy.3@gmail.com>
2025-07-16 11:27:18 +0300

    debug : toString in Grid

--- src/main/java/com/monorganisation/monprojet/Grid.java
+++ src/main/java/com/monorganisation/monprojet/Grid.java
@@ -3,22 +3,36 @@ package com.monorganisation.monprojet;
 import java.util.Arrays;
 
 public class Grid {
-
-     static char[][] initGrid(int size, int Junk){
+    char[][] array;
+     Grid(int size, int Junk){
         char[][] matrice = new char[size][size];
          for (int i = 0; i < size; i++) {
              Arrays.fill(matrice[i], '.');
          }
          for (int i = 0; i < Junk; i++) {
-             int X = (int)(Math.random() * size);
-             int Y = (int)(Math.random() * size);
-             matrice[X][Y] = 'J';
+             int X = (int)(Math.random() * size-1);
+             int Y = (int)(Math.random() * size-1);
+             matrice[X+1][Y+1] = 'J';
          }
-        return matrice;
+         this.array = matrice;
+    }
+
+    @Override
+    public String toString(){
+        String res = "";
+        int i = 0, j = 0;
+        for(i=0;i<5;i++){
+            for(j=0;j<5;j++){
+                res+=((array[i][j]));
+            }
+            res+= "\n";
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.deepToString(initGrid(5, 5)));
+         Grid test = new Grid(5,5);
+        System.out.println(test.toString());
     }
 
 
